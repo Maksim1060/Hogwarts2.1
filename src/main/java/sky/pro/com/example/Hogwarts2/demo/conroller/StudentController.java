ad56e1176a610e5db2975f75d1c5e5c60a747323
Maksim <maks.kartashov.93@inbox.ru>
2024-01-21 23:15:34 +0200

    swagger

---
+++ src/main/java/sky/pro/com/example/Hogwarts2/demo/conroller/StudentController.java
@@ -0,0 +1,33 @@
+package sky.pro.com.example.Hogwarts2.demo.conroller;
+
+import org.springframework.web.bind.annotation.*;
+import sky.pro.com.example.Hogwarts2.demo.model.Student;
+import sky.pro.com.example.Hogwarts2.demo.service.StudentServiceImpl;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private final StudentServiceImpl studentService;
+
+    public StudentController(StudentServiceImpl studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable long id) {
+        return studentService.getStudent(id);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+@PutMapping("/{id}")
+    public Student updateStudent(@PathVariable long id,@RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+@DeleteMapping("/{id}")
+    public void removeStudent(@PathVariable long id) {
+         studentService.removeStudent(id);
+    }
+}
